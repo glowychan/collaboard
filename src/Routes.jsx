d7ec2226007114c2d7de80c65d9c507ab5ecfe8e
glowychan <glochan.uog@gmail.com>
2017-09-05 08:53:14 -0400

    Add random url generator

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SketchApp from './layouts/SketchApp';
 
+const randomStringFunction = function() {
+  let id = "";
+  const source = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    id += source.charAt(Math.floor(Math.random() * source.length));
+  }
+  return id;
+};
+
 const Home = () => (
   <div>
     <h1>Insert Home page component from layouts folder</h1>
   </div>
 )
 
-const Whiteboard = ({ match }) => (
+const Twoodle = ({ match }) => (
   <div>
     <h3>{match.params.boardId}</h3>
     <SketchApp />
   </div>
 );
 
-const Whiteboards = ({ match }) => (
+const Twoodles = ({ match }) => (
   <div>
-    <h3>Generate new board</h3>
-    <Route path={`${match.url}/:boardId`} component={Whiteboard}/>
+    <h3>Create a New Twoodle</h3>
+    <a href={`${match.url}/${randomStringFunction()}`}>Link!!</a>
+    <Route path={`${match.url}/:boardId`} component={Twoodle}/>
   </div>
 )
 
@@ -27,13 +37,13 @@ const Routes = () => (
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/whiteboards">Whiteboards</Link></li>
+        <li><Link to="/twoodles">Twoodles</Link></li>
       </ul>
 
       <hr/>
 
       <Route exact path="/" component={Home}/>
-      <Route path="/whiteboards" component={Whiteboards}/>
+      <Route path="/twoodles" component={Twoodles}/>
     </div>
   </Router>
 )
