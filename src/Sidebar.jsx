bd61805bbd13d4bbe8679f4f0c116f3ffae1c0f4
Amy <amy@lighthouselabs.ca>
2017-09-05 13:15:38 +0000

    Removed bundle

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,22 +4,17 @@ import { push as Menu } from 'react-burger-menu'
 class SideBar extends Component {
   render () {
     return (
-<<<<<<< HEAD
-    <Menu className="bm-menu" pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } >
-=======
-
     <Menu className="bm-menu" 
     pageWrapId={ "page-wrap" } 
     outerContainerId={ "outer-container" }
     width={ '20%' } >
->>>>>>> feature/HMR
         <a className="bm-item-list" to="/">One</a>
         <a className="bm-item-list" to="/courses">Two</a>
         <a className="bm-item-list" to="/about">Three</a>
     </Menu>
 
     );
-};
+  };
 };
 
 
