11c7a87e1d4dd319c2a2ee6bcfb564d1c875b5ab
Rayhaneh <r.b.assady@gmail.com>
2017-09-05 17:51:50 -0400

    Added websocket functionality. Removed dead code. Changed boardId to boardName

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -18,83 +18,56 @@ const Home = () => (
   </div>
 )
 
-// ajax call inside the Home to /?query, the query will come from the form
-//form on the home page
-
-/*
-class Twoodle extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {url: ''}
-  }
-
-  componentWillMount() {
-    new websocket
-    this.
-    onmessage....setstate (messagetype: newUrl)
-    check if need to redirect
-
-    this.state....
-  }
-
-  render() {
-    return (
-      <div>
-      </div>
-    )
-  }
-}
-*/
-
 
 class Twoodle extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      boardId: props.match.params.boardId,
+      boardName: props.match.params.boardName,
       items: []
     }
   }
   componentDidMount() {
-    // websokcket connection with bordid
-    // get items
-    this.setState({items: []})
+    this.socket = new WebSocket("ws://localhost:3001")
+    this.socket.onmessage = (receivedData) => {
+      const data = JSON.parse(receivedData.data)
 
+      if (data.boardName === this.state.boardName) {
+        console.log(this.state.items)
+        this.setState({items: [...this.state.items, data.item]})
+      }
+    }
   }
   render() {
     return (
       <div>
-        <h3>Your twoodle code is {this.state.boardId}</h3>
-        <SketchApp items ={this.state.items} />
+        <h3>Your twoodle bord name is: {this.state.boardName}</h3>
+        <SketchApp items ={this.state.items}
+                   boardName = {this.state.boardName}
+                   addNewItem = {this.addNewItem}/>
       </div>
     )
   }
+
+  addNewItem = (item, boardName) => {
+    const data = {
+      boardName: boardName,
+      item:  item
+    }
+    this.socket.send(JSON.stringify(data))
+  }
 }
-/*
-const Twoodles = ({ match }) => (
-  <div>
-    <Link to={`${match.url}/${randomStringFunction()}`}><h3>New Twoodle</h3></Link>
-    <Route path={`${match.url}/:boardId`} component={Twoodle}/>
-  </div>
-)
-*/
+
 class Twoodles extends React.Component {
 
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {url: ''}
-  // }
 
-  // componentWillMount() {
-  //   this.setState({url: 123})
-  // }
   //REDIRECT TWOODLES PAGE TO MAIN PAGE
   render() {
     return (
       <div>
         {console.log(this.props.match.url)}
         <Link to={`${this.props.location.pathname}`}><h3>New Twoodle</h3></Link>
-        <Route path={`${this.props.match.url}/:boardId`} component={Twoodle}/>
+        <Route path={`${this.props.match.url}/:boardName`} component={Twoodle}/>
       </div>
     )
   }
