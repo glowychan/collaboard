5dfd83e51ee7f25b005fa020fa58c611bbb41b57
glowychan <glochan.uog@gmail.com>
2017-09-05 09:26:21 -0400

    revert commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SketchApp from './SketchApp';
+import SketchApp from './layouts/SketchApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<SketchApp />, document.getElementById('root'));
