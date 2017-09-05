093e87ec48d55d8082823258788aaa5695ac1991
glowychan <glochan.uog@gmail.com>
2017-09-05 11:43:45 -0400

    refactor

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -17,19 +17,65 @@ const Home = () => (
   </div>
 )
 
+/*
+class Twoodle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {url: ''}
+  }
+
+  componentWillMount() {
+    new websocket
+    this.
+    onmessage....setstate (messagetype: newUrl)
+    check if need to redirect
+
+    this.state....
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+*/
+
+
 const Twoodle = ({ match }) => (
   <div>
     <h3>Your twoodle code is {match.params.boardId}</h3>
     <SketchApp />
   </div>
 );
-
+/*
 const Twoodles = ({ match }) => (
   <div>
     <Link to={`${match.url}/${randomStringFunction()}`}><h3>New Twoodle</h3></Link>
     <Route path={`${match.url}/:boardId`} component={Twoodle}/>
   </div>
 )
+*/
+class Twoodles extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {url: ''}
+  }
+
+  componentWillMount() {
+    this.setState({url: 123 })
+  }
+
+  render() {
+    return (
+      <div>
+        <Link to={`${this.props.match.url}/${this.state.url}`}><h3>New Twoodle</h3></Link>
+        <Route path={`${this.props.match.url}/:boardId`} component={Twoodle}/>
+      </div>
+    )
+  }
+}
 
 const Routes = () => (
   <Router>
