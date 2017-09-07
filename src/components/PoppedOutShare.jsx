fc05247dc566a2c1a77d2f9a75072a2cbe399175
notadeejay <amy@lighthouselabs.ca>
2017-09-07 14:48:50 -0400

    Added social sharing buttons

--- src/components/PoppedOutShare.jsx
+++ src/components/PoppedOutShare.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { ShareButtons, generateShareIcon } from 'react-share';
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const EmailIcon = generateShareIcon('email');
+const LinkedinIcon = generateShareIcon('linkedin');
+
+const {
+            FacebookShareButton,
+            GooglePlusShareButton,
+            LinkedinShareButton,
+            TwitterShareButton,
+            TelegramShareButton,
+            WhatsappShareButton,
+            PinterestShareButton,
+            VKShareButton,
+            OKShareButton,
+            RedditShareButton,
+            EmailShareButton,
+          } = ShareButtons;
+
 
 class PoppedOutShare extends React.Component {
   constructor(props) { 
     super(props);
-console.log(this.props.url)
+
     this.setWrapperRef = this.setWrapperRef.bind(this);           
     this.handleClickOutside = this.handleClickOutside.bind(this);
   }
-  
   
   componentDidMount() {
         document.addEventListener('mousedown', this.handleClickOutside);
@@ -27,16 +47,31 @@ console.log(this.props.url)
           this.props.onClose()
       }
   }
-
-
+   
   render() {
     if(this.props.isOpen === false)
     return null 
 
     return (
       <div ref={this.setWrapperRef} className='share-pop'>
+      <div className='url-share'>
       <h2>Share your Twoodle URL and draw with friends</h2>
         <input className='share-url' id='share' type='text' defaultValue={'https://twoodle-board.com/' + this.props.url} readOnly /> 
+      </div>
+      <div className='social-share'>
+        <FacebookShareButton title="Twoodle" quote="Check out my latest Twoodle" url={'https://twoodle-board.com/' + this.props.url}>
+          <FacebookIcon size={60} round />
+        </FacebookShareButton>
+        <TwitterShareButton>
+          <TwitterIcon size={60} round/>
+        </TwitterShareButton>
+        <LinkedinShareButton>
+          <LinkedinIcon size={60} round/>
+        </LinkedinShareButton>
+        <EmailShareButton>
+          <EmailIcon size={60} round/>
+        </EmailShareButton>
+      </div>
       </div>
     )
   }
