4a5194c45b1f4d3763556971977490867b2a2e93
Rayhaneh <r.b.assady@gmail.com>
2017-09-10 17:52:41 -0400

    Cleared the code from dead code

--- server/server.js
+++ server/server.js
@@ -1,34 +1,21 @@
-const app = require('express')();
+const app    = require('express')();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const cors         = require('cors')
+const io     = require('socket.io')(server);
+const cors   = require('cors')
 
 require('dotenv').config()
 
 
 // Set the port to 3001
 const PORT = 3001
-
 server.listen(PORT);
 
 // Express Middlewares
 app.use(cors())
 
-// Create Express App
-// const server = app
-// .use(express.static('public'))
-// .listen(
-//   PORT, '0.0.0.0', 'localhost',
-//   () => console.log(`Listening on ${ PORT }`)
-//   )
-
-
 // Database Connection
 const MongoClient = require('mongodb').MongoClient
 const MONGODB_URI = process.env.MONGODB_URI
-
-// Create WebSockets server
-// const wss = new SocketServer({ server });
 
 // Connect to the database
 MongoClient.connect(MONGODB_URI)
@@ -43,13 +30,8 @@ MongoClient.connect(MONGODB_URI)
     app.use('/', routes)
 
 
+    // An interface to websockets
     require('./socket-io',)(io, dataHelpers)
-
-
-
-    // An interface to the websockets
-    // const socketHelpers = require('./socketHelpers.js')(wss)
-    // require('./sockets')(wss, socketHelpers, dataHelpers)
 
   })
   .catch((err) => {
