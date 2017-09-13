7e125ce2b1c019d9d53509b56f97a528d67e01da
glowychan <glochan.uog@gmail.com>
2017-09-13 09:55:27 -0400

    clean server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-const app    = require('express')();
-const server = require('http').Server(app);
-const io     = require('socket.io')(server);
+const app    = require('express')()
+const server = require('http').Server(app)
+const io     = require('socket.io')(server)
 const cors   = require('cors')
 
 require('dotenv').config()
 
-
 // Set the port to 3001
 const PORT = 3001
-server.listen(PORT);
+server.listen(PORT)
 
 // Express Middlewares
 app.use(cors())
@@ -19,25 +18,20 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 // Connect to the database
 MongoClient.connect(MONGODB_URI)
-  .then ((db) => {
+  .then (db => {
     console.log(`Connected to mongodb: ${MONGODB_URI}`)
 
-    // A interface to the database
+    // An interface to the database
     const dataHelpers = require('./db/data-helpers.js')(db)
 
     // An interface to the routes
     const routes = require('./routes.js')(dataHelpers)
     app.use('/', routes)
 
-
     // An interface to websockets
     require('./socket-io')(io, dataHelpers)
-
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(`Failed to connect: ${MONGODB_URI}`)
     throw err
   })
-
-
-
