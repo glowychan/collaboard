520370e359aeeae9349bfc2b64894b546f33f185
glowychan <glochan.uog@gmail.com>
2017-09-13 13:26:03 -0400

    clean username popout

--- src/components/UserNamePopout.jsx
+++ src/components/UserNamePopout.jsx
@@ -2,24 +2,21 @@ import React from 'react'
 import About from '../icons/About.png'
 
 class UserNamePopout extends React.Component {
-  constructor(props) { 
-    super(props);
-
+  constructor(props) {
+    super(props)
   }
- 
-   
+
   render() {
-    
-    if(this.props.isOpen === false)
-    return null 
+    if(!this.props.isOpen)
+    return null
 
     return (
       <div ref={this.setWrapperRef} className='name-pop'>
-          <h2 className='name-label'>Enter your name:</h2>
-          <form className='new-user'onSubmit={this.props.onClose}>
-          <input className='user-name' id='user' name="userName"/>
-          <button type='submit' className='user-submit'></button>
-          </form>
+        <h2 className='name-label'>Enter your name:</h2>
+        <form className='new-user'onSubmit={this.props.onClose}>
+        <input className='user-name' id='user' name="userName"/>
+        <button type='submit' className='user-submit'></button>
+        </form>
      </div>
     )
   }
