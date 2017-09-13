458ae54ab0c9ced6a2658fb9cc06700c3f2b04f4
glowychan <glochan.uog@gmail.com>
2017-09-13 12:15:51 -0400

    clean sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { slide as Menu } from 'react-burger-menu'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import axios from 'axios';
-import home from '../icons/003-symbols.png';
-import newtwoodle from '../icons/002-symbol-2.png';
-import save from '../icons/001-symbols-1.png';
-import share from '../icons/004-sign.png';
-import trashcan from '../icons/006-symbol-1.png';
-import PoppedOutShare from './PoppedOutShare';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+import home from '../icons/003-symbols.png'
+import newtwoodle from '../icons/002-symbol-2.png'
+import save from '../icons/001-symbols-1.png'
+import share from '../icons/004-sign.png'
+import trashcan from '../icons/006-symbol-1.png'
+import PoppedOutShare from './PoppedOutShare'
 
 
 class SideBar extends Component {
   constructor(props) {
     super(props)
-    this.popout = this.popout.bind(this);
-    this.onDeleteClick = this.onDeleteClick.bind(this);
+    this.popout = this.popout.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
   }
 
    popout() {
-    this.props.onShare();
+    this.props.onShare()
   }
 
   onDeleteClick = (event) => {
@@ -29,8 +29,8 @@ class SideBar extends Component {
         window.location = `/`;
       })
       .catch((error) => {
-        console.log("error");
-      });
+        console.log("error", error)
+      })
     }
 
   render () {
@@ -41,13 +41,11 @@ class SideBar extends Component {
     width={ '4%' } >
         <Link className="bm-item-list side-item" to="/"><img className='home' src={home} /></Link>
         <Link className="bm-item-list side-item" to="/"><img className='twoodle' src={newtwoodle} /></Link>
-        <a href="#" className='bm-item-list side-item' onClick={this.popout}><img src={share} /> </a>
+        <a className='bm-item-list side-item' onClick={this.popout}><img src={share} /> </a>
         <a className='bm-item-list side-item' onClick={this.onDeleteClick}><img src={trashcan} /> </a>
     </Menu>
-
     );
   };
 };
 
-
-export default SideBar;
+export default SideBar
