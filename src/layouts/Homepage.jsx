ed1f536a41ffc365b7e26b9045713af2bc7d37f7
notadeejay <amy@lighthouselabs.ca>
2017-09-13 18:09:49 -0400

    Fixed toolbar css

--- src/layouts/Homepage.jsx
+++ src/layouts/Homepage.jsx
@@ -3,7 +3,11 @@ import AboutPopout from '../components/AboutPopout';
 import axios from 'axios';
 import brand from '../icons/TWOODLE.png';
 import about from '../icons/Howitworks.png';
-
+import woman from '../icons/woman.png'
+import facebook from '../icons/facebook.png'
+import twitter from '../icons/twitter.png'
+import instagram from '../icons/instagram.png'
+import alert from '../icons/alert.png'
 
 export default class Homepage extends Component {
 
@@ -45,7 +49,8 @@ export default class Homepage extends Component {
           window.location = `/twoodles/${boardName}`;
         })
         .catch((error) => {
-          this.setState({error: 'Twoodle name is already taken.'});
+          this.setState({error: 'Twoodle name is already taken. Please try another one.'});
+          this.refs.boardName.value="";
         });
 
      }
@@ -58,17 +63,29 @@ export default class Homepage extends Component {
   render() {
      return (
      <div className='main-container'> 
+     {this.state.error ? 
+      <div className='error-message' >
+        <p>{this.state.error}</p>
+      </div> : ''}
       <div className='jumbotron'>
       <img className='home-logo' src={brand} />
         <form className='new-twoodle'
         onSubmit={this.submitForm}>
-          <input className='twoodle-name' name="boardName"/>
+          <input className='twoodle-name' name="boardName" ref='boardName'/>
           <button className='twoodle-submit'>submit</button>
           <img className='about' onClick={this.openPopup} src={about} />
            <AboutPopout onClose={this.closePopup} isOpen={this.state.poppedOpen} />
         </form>
         <h2 className='slogan'>Welcome to your whiteboard <span className='purple'>on the web</span></h2>
-        <p>{this.state.error}</p>
+        <footer className='footer'>
+          <p className='copy'> ©2017 Twoodle Inc.</p>
+          <div className= 'built-by'>
+            <p>Follow us on </p>
+            <img src={facebook} />
+            <img src={instagram} />
+            <img src={twitter} />
+          </div>
+        </footer>
       </div>
     </div>
     );
