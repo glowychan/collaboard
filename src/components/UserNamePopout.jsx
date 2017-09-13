ac82eba0ae0ec83a4290b02492b15a5e0709fc39
glowychan <glochan.uog@gmail.com>
2017-09-13 13:46:10 -0400

    add notes to user name popout

--- src/components/UserNamePopout.jsx
+++ src/components/UserNamePopout.jsx
@@ -22,4 +22,19 @@ class UserNamePopout extends React.Component {
   }
 }
 
+// This should be a functional component because state is not stored
+// But isOpen does not work
+// const UserNamePopout = ({ isOpen, setWrapperRef, onClose }) => (
+//   if (!isOpen) {
+//     return null;
+//   }
+//   <div ref={setWrapperRef} className='name-pop'>
+//     <h2 className='name-label'>Enter your name:</h2>
+//     <form className='new-user'onSubmit={onClose}>
+//     <input className='user-name' id='user' name="userName"/>
+//     <button type='submit' className='user-submit'></button>
+//     </form>
+//   </div>
+// )
+
 export default UserNamePopout
