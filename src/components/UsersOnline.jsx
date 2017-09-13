1ad5e54ac44c9f7a070e7c68e3467fb74fb717b6
glowychan <glochan.uog@gmail.com>
2017-09-13 13:59:59 -0400

    change users online to functional component

--- src/components/UsersOnline.jsx
+++ src/components/UsersOnline.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 
+// class UsersOnline extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
+//   // randomColor = () => {
+//   //  return '#' + (0x1000000 + Math.random() * 0xFFFFFF).toString(16).substr(1,6);
+//   // }
 
-class UsersOnline extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+//   render() {
+//     const users = this.props.users
+//     return (
+//     <div className='user-container'>
+//       {users.map((user) => {
+//         return <div className='online-user' style={{background: '#6ED3CF'}} key={user.id}>
+//           <p> {user.name} </p>
+//         </div>
+//       })}
+//     </div>
+//     );
+//   }
+// }
 
-  // randomColor = () => {
-  //  return '#' + (0x1000000 + Math.random() * 0xFFFFFF).toString(16).substr(1,6);
-  // }
-
-
-
-  render() {
-    const users = this.props.users
-    return (
-    <div className='user-container'>
-      {users.map((user) => {
-        return <div className='online-user' style={{background: '#6ED3CF'}} key={user.id}>
-          <p> {user.name} </p>
-        </div>
-      })}
-    </div>
-    );
-  }
-}
+// If not storing state, should use functional component
+const UsersOnline = ({ users }) => (
+  <div className='user-container'>
+    {users.map((user) => {
+      return <div className='online-user' style={{background: '#6ED3CF'}} key={user.id}>
+        <p> {user.name} </p>
+      </div>
+    })}
+  </div>
+)
 
 export default UsersOnline
