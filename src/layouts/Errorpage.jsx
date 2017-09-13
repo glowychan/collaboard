9390b44434ed777601992ff8a34b8ee0e2246362
glowychan <glochan.uog@gmail.com>
2017-09-13 14:06:02 -0400

    clean errorpage

--- src/layouts/Errorpage.jsx
+++ src/layouts/Errorpage.jsx
@@ -1,42 +1,36 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Error extends Component {
-
-constructor(props) {
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       url:''
     }
-
     this.getGif = this.getGif.bind(this)
- }
+  }
 
- componentDidMount() {
-   this.getGif();
- }
+  componentDidMount() {
+    this.getGif();
+  }
 
- getGif = () => {
-   console.log('getgif ', this.state)
-   axios.get(`http://api.giphy.com/v1/gifs/random?api_key=df85a405d9194efcb483d6d77c5dcba7&tag=looking&rating=g`)
-          .then((response) => {
-            this.setState({
-              url:response.data.data.image_url
-            })
-          })
-          .catch((error) => {
-            console.log('Whoops.. No Gif for you!')
+  getGif = () => {
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=df85a405d9194efcb483d6d77c5dcba7&tag=looking&rating=g`)
+         .then((response) => {
+            this.setState({ url:response.data.data.image_url })
+         })
+         .catch((error) => {
+            console.log("error", error)
           });
- }
-  
+  }
 
-render() {
-     return (
-     <div className='error-container'>
-      <span className='four'> FOUR<a className='oh'>OH</a>FOUR</span>
-        <span className='error-msg'>We are looking for your page... but can't find it</span>
-        <img className='gif' src={this.state.url} />
-    </div>
+  render() {
+    return (
+      <div className='error-container'>
+        <span className='four'> FOUR<a className='oh'>OH</a>FOUR</span>
+          <span className='error-msg'>We are looking for your page... but can't find it</span>
+          <img className='gif' src={this.state.url} />
+      </div>
     );
   }
 
