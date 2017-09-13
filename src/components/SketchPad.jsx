5344d37a863494acd0670954354f5dcab74bd48b
glowychan <glochan.uog@gmail.com>
2017-09-13 12:55:18 -0400

    clean sketchpad

--- src/components/SketchPad.jsx
+++ src/components/SketchPad.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { findDOMNode } from 'react-dom';
-import Pencil, { TOOL_PENCIL } from './tools/Pencil';
-import Line, { TOOL_LINE } from './tools/Line';
-import Ellipse, { TOOL_ELLIPSE} from './tools/Ellipse';
-import Rectangle, { TOOL_RECTANGLE } from './tools/Rectangle';
-import Brush, { TOOL_BRUSH } from './tools/Brush';
-import Eraser, { TOOL_ERASER } from './tools/Eraser';
-import FileSaver from 'file-saver';
-
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { findDOMNode } from 'react-dom'
+import Pencil, { TOOL_PENCIL } from './tools/Pencil'
+import Line, { TOOL_LINE } from './tools/Line'
+import Ellipse, { TOOL_ELLIPSE} from './tools/Ellipse'
+import Rectangle, { TOOL_RECTANGLE } from './tools/Rectangle'
+import Brush, { TOOL_BRUSH } from './tools/Brush'
+import Eraser, { TOOL_ERASER } from './tools/Eraser'
+import FileSaver from 'file-saver'
 
 export const toolsMap = {
   [TOOL_PENCIL]: Pencil,
@@ -20,9 +19,8 @@ export const toolsMap = {
 };
 
 export default class SketchPad extends Component {
-
-  tool = null;
-  interval = null;
+  tool = null
+  interval = null
 
   static propTypes = {
     width: PropTypes.number,
@@ -40,7 +38,7 @@ export default class SketchPad extends Component {
     onDebouncedItemChange: PropTypes.func,
     onCompleteItem: PropTypes.func,
     debounceTime: PropTypes.number,
-  };
+  }
 
   static defaultProps = {
     // width: 500,
@@ -53,41 +51,36 @@ export default class SketchPad extends Component {
     animate: false,
     tool: TOOL_PENCIL,
     toolsMap
-  };
+  }
 
   constructor(props) {
-    super(props);
-
-    
-    this.initTool = this.initTool.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onDebouncedMove = this.onDebouncedMove.bind(this);
-    this.onMouseUp = this.onMouseUp.bind(this);
-    this._onTouchStart = this._onTouchStart.bind(this);
-    this._onTouchMove = this._onTouchMove.bind(this);
-    this._onTouchEnd = this._onTouchEnd.bind(this);
+    super(props)
+    this.initTool = this.initTool.bind(this)
+    this.onMouseDown = this.onMouseDown.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onDebouncedMove = this.onDebouncedMove.bind(this)
+    this.onMouseUp = this.onMouseUp.bind(this)
+    this._onTouchStart = this._onTouchStart.bind(this)
+    this._onTouchMove = this._onTouchMove.bind(this)
+    this._onTouchEnd = this._onTouchEnd.bind(this)
     this.updateDimensions = this.updateDimensions.bind(this)
     this.redraw = this.redraw.bind(this)
-
-  
   }
 
   componentDidMount() {
-    this.canvas = findDOMNode(this.canvasRef);
-    this.ctx = this.canvas.getContext('2d');
-    this.canvas.width = this.canvas.offsetWidth;
+    this.canvas = findDOMNode(this.canvasRef)
+    this.ctx = this.canvas.getContext('2d')
+    this.canvas.width = this.canvas.offsetWidth
     this.canvas.height = this.canvas.offsetHeight
     this.initialHeight = this.canvas.height
     this.initialWidth = this.canvas.width
-    this.initTool(this.props.tool);
-    this.ctx.fillStyle = 'white';
+    this.initTool(this.props.tool)
+    this.ctx.fillStyle = 'white'
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-  
     window.addEventListener('resize', this.updateDimensions)
   }
 
-  updateDimensions () {
+  updateDimensions() {
     this.canvas.width = this.canvas.offsetWidth;
     this.canvas.height = this.canvas.offsetHeight
     this.redraw()
@@ -96,46 +89,40 @@ export default class SketchPad extends Component {
   redraw() {
     this.props.items
       .forEach(item => {
-        this.initTool(item.tool);
-        this.tool.draw(item, this.props.animate);
-      });
-    this.initTool(this.props.tool);
+        this.initTool(item.tool)
+        this.tool.draw(item, this.props.animate)
+      })
+    this.initTool(this.props.tool)
    }
-  
- 
 
   _onTouchStart(e) {
-    const data = this.tool.onMouseDown(...this.getCursorPosition(e.touches[0]), this.props.color, this.props.size, this.props.fillColor);
-    data && data[0] && this.props.onItemStart && this.props.onItemStart.apply(null, data);
+    const data = this.tool.onMouseDown(...this.getCursorPosition(e.touches[0]), this.props.color, this.props.size, this.props.fillColor)
+    data && data[0] && this.props.onItemStart && this.props.onItemStart.apply(null, data)
     if (this.props.onDebouncedItemChange) {
-      this.interval = setInterval(this.onDebouncedMove, this.props.debounceTime);
+      this.interval = setInterval(this.onDebouncedMove, this.props.debounceTime)
     }
   }
 
   _onTouchMove(e) {
-    const data = this.tool.onMouseMove(...this.getCursorPosition(e.touches[0]));
-    data && data[0] && this.props.onEveryItemChange && this.props.onEveryItemChange.apply(null, data);
+    const data = this.tool.onMouseMove(...this.getCursorPosition(e.touches[0]))
+    data && data[0] && this.props.onEveryItemChange && this.props.onEveryItemChange.apply(null, data)
   }
 
   _onTouchEnd(e) {
-    const data = this.tool.onMouseUp(...this.getCursorPosition(e.changedTouches[0]));
-    data && data[0] && this.props.onCompleteItem && this.props.onCompleteItem.apply(null, data);
+    const data = this.tool.onMouseUp(...this.getCursorPosition(e.changedTouches[0]))
+    data && data[0] && this.props.onCompleteItem && this.props.onCompleteItem.apply(null, data)
     if (this.props.onDebouncedItemChange) {
-      clearInterval(this.interval);
-      this.interval = null;
+      clearInterval(this.interval)
+      this.interval = null
     }
   }
 
-
-
-handleSave = () => {
-  const userinput = prompt("Please enter a filename");
-  const filename = userinput.concat('');
-
-   this.canvas.toBlob(function(blob) {
-    FileSaver.saveAs(blob, `${filename}.jpg`);
-    });
-
+  handleSave = () => {
+    const userinput = prompt("Please enter a filename")
+    const filename = userinput.concat('')
+    this.canvas.toBlob(function(blob) {
+      FileSaver.saveAs(blob, `${filename}.jpg`)
+    })
   }
 
   handleClear = () => {
@@ -144,68 +131,64 @@ handleSave = () => {
 
   componentWillReceiveProps({ tool, items }) {
     items
-      .filter(item => this.props.items.indexOf(item) === -1)
-      .forEach(item => {
-        this.initTool(item.tool);
-        this.tool.draw(item, this.props.animate);
-      });
-    this.initTool(this.props.tool);
+    .filter(item => this.props.items.indexOf(item) === -1)
+    .forEach(item => {
+      this.initTool(item.tool)
+      this.tool.draw(item, this.props.animate)
+    })
+    this.initTool(this.props.tool)
   }
 
-
   initTool(tool) {
-    this.tool = this.props.toolsMap[tool](this.ctx);
+    this.tool = this.props.toolsMap[tool](this.ctx)
   }
 
   onMouseDown(e) {
-    const data = this.tool.onMouseDown(...this.getCursorPosition(e), this.props.color, this.props.size, this.props.fillColor);
-    data && data[0] && this.props.onItemStart && this.props.onItemStart.apply(null, data);
+    const data = this.tool.onMouseDown(...this.getCursorPosition(e), this.props.color, this.props.size, this.props.fillColor)
+    data && data[0] && this.props.onItemStart && this.props.onItemStart.apply(null, data)
     if (this.props.onDebouncedItemChange) {
-      this.interval = setInterval(this.onDebouncedMove, this.props.debounceTime);
+      this.interval = setInterval(this.onDebouncedMove, this.props.debounceTime)
     }
   }
 
   onDebouncedMove() {
     if (typeof this.tool.onDebouncedMouseMove === 'function' && this.props.onDebouncedItemChange) {
-      this.props.onDebouncedItemChange.apply(null, this.tool.onDebouncedMouseMove());
+      this.props.onDebouncedItemChange.apply(null, this.tool.onDebouncedMouseMove())
     }
   }
 
   onMouseMove(e) {
-    const data = this.tool.onMouseMove(...this.getCursorPosition(e));
-    data && data[0] && this.props.onEveryItemChange && this.props.onEveryItemChange.apply(null, data);
+    const data = this.tool.onMouseMove(...this.getCursorPosition(e))
+    data && data[0] && this.props.onEveryItemChange && this.props.onEveryItemChange.apply(null, data)
   }
 
   onMouseUp(e) {
-    const data = this.tool.onMouseUp(...this.getCursorPosition(e));
-    data && data[0] && this.props.onCompleteItem && this.props.onCompleteItem.apply(null, data);
+    const data = this.tool.onMouseUp(...this.getCursorPosition(e))
+    data && data[0] && this.props.onCompleteItem && this.props.onCompleteItem.apply(null, data)
     if (this.props.onDebouncedItemChange) {
-      clearInterval(this.interval);
-      this.interval = null;
+      clearInterval(this.interval)
+      this.interval = null
     }
   }
 
   getCursorPosition(e) {
-    const { top, left } = this.canvas.getBoundingClientRect();
-   
+    const { top, left } = this.canvas.getBoundingClientRect()
     // const ratioH = this.initialHeight/this.canvas.height
     // const ratioW = this.initialWidth/this.canvas.width
-
-    return [
+    return ([
       e.clientX - left,
       e.clientY - top
-    ];
+    ])
   }
 
-  _handleTouchStart =() => {
-    console.log('handleTouchStart');
+  _handleTouchStart = () => {
+    console.log('handleTouchStart')
   }
 
   render() {
     const { width, height, canvasClassName } = this.props;
     return (
       <div>
-
         <div className="canvas-div">
           <canvas
             ref={(canvas) => { this.canvasRef = canvas; }}
