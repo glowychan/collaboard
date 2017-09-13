a194501b1807073a3336a694f98a0cade0beb80c
glowychan <glochan.uog@gmail.com>
2017-09-13 10:50:45 -0400

    clean about popout

--- src/components/AboutPopout.jsx
+++ src/components/AboutPopout.jsx
@@ -1,41 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import about from '../icons/About.png'
 
-class AboutPopout extends React.Component {
-  constructor(props) { 
+class AboutPopout extends Component {
+  constructor(props) {
     super(props);
-
-    this.setWrapperRef = this.setWrapperRef.bind(this);           
+    this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
   }
-  
+
   componentDidMount() {
-      document.addEventListener('mousedown', this.handleClickOutside);
-    }
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
 
   componentWillUnmount() {
-      document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutside);
   }
 
   setWrapperRef(node) {
-      this.wrapperRef = node;
-    }
+    this.wrapperRef = node;
+  }
 
   handleClickOutside(event) {
-      if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-          this.props.onClose()
-      }
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.props.onClose()
+    }
   }
-   
-  render() {
-    
-    if(this.props.isOpen === false)
-    return null 
 
+  render() {
+    if(!this.props.isOpen)
+    return null
     return (
       <div ref={this.setWrapperRef} className='about-pop'>
-         <img src={about} />
-    </div>
+        <img src={about} />
+      </div>
     )
   }
 }
