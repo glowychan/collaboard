2cbab3c3e8608f36df9f008984235d3cf60693ed
Rayhaneh <r.b.assady@gmail.com>
2017-09-16 22:13:00 -0400

    Removed dead code

--- src/components/tools/Textbox.js
+++ src/components/tools/Textbox.js
@@ -46,62 +46,8 @@ export default (context) => {
           mouseY += lineHeight
         }
 
-        // words.forEach((word) => {
-        //   const metrics = context.measureText(word)
-        //   const wordWidth = metrics.width
-        //   console.log(word, wordWidth, spaceLeft)
-        //   if (wordWidth < spaceLeft) {
-        //     tempLine  += word + ' '
-        //     spaceLeft -= wordWidth
-        //     console.log(tempLine, 'spaceLeft: ', spaceLeft)
-        //   }
-        //   context.strokeText(tempLine, mouseX, mouseY)
-        //   tempLine = ''
-        //   spaceLeft = maxWidth
-        //   mouseY += lineHeight
-          // if (wordWidth < spaceLeft) {
-          //   tempLine += word + ' '
-          //   spaceLeft -= wordWidth
-          // }
-          // else {
-          //   printLine =
-          //   console.log(word, wordWidth, tempLine, spaceLeft)
-          //   // context.strokeText(tempLine, mouseX, mouseY)
-          //   tempLine = ''
-          //   spaceLeft = maxWidth
-          //   mouseY += lineHeight
-          // }
-        // })
       })
-
-
     }
-    // if (item.text) {
-    //   const words = item.text.trim().split(' ')
-    //   console.log('words', words)
-
-    //   let line = ''
-    //   for (let n = 0; n < words.length; n++) {
-    //     console.log('word is: ',words[n])
-    //     const testLine = line + words[n] + ' ';
-    //     const metrics = context.measureText(testLine);
-    //     const testWidth = metrics.width;
-    //     if (testWidth > maxWidth && n > 0) {
-    //       context.font = '14px Comic Sans MS'
-    //       console.log('line is: ',line)
-    //       context.strokeText(line, mouseX, mouseY)
-    //       line = words[n] + ' ';
-    //       mouseY += lineHeight;
-    //     }
-    //     else {
-    //       line = testLine;
-    //     }
-    //     console.log(n, words.length, testLine, line)
-    //     if (n === (words.length - 1) && testLine !== line) {
-    //       context.strokeText(words[n], mouseX, mouseY)
-    //     }
-    //   }
-    // }
   }
 
 
