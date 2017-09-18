aac0976575ee0c2caf4d6823ada3eeb6f6dd41bb
glowychan <glochan.uog@gmail.com>
2017-09-18 14:25:40 -0400

    clean color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { CirclePicker } from 'react-color'
 
 class ColorPicker extends React.Component {
   state = {
     background: '#fff',
-  };
+  }
 
-  handleChangeComplete = (color) => {
-    this.setState({ background: color.hex });
+  handleChangeComplete = color => {
+    this.setState({ background: color.hex })
     this.props.newColor(this.state.background)
-  };
+  }
 
   render() {
     return (
       <CirclePicker
-        color={ this.state.background } 
-        width={155} 
+        color={this.state.background}
+        width={155}
         circleSize={13}
-        colors={["#FF0000", "#FFC300", "#529749", "#2196f3",  "#000000"]}
-        onChangeComplete={ this.handleChangeComplete }
+        colors={["#FF0000", "#FFC300", "#529749", "#2196f3", "#000000"]}
+        onChangeComplete={this.handleChangeComplete}
       />
-    );
+    )
   }
 }
 
