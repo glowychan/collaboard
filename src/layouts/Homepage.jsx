f607ffe55378b675c4b92e492047099cbc9620ae
glowychan <glochan.uog@gmail.com>
2017-09-18 15:48:57 -0400

    clean homepage

--- src/layouts/Homepage.jsx
+++ src/layouts/Homepage.jsx
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
-import AboutPopout from '../components/AboutPopout';
-import axios from 'axios';
-import brand from '../icons/TWOODLE.png';
-import about from '../icons/Howitworks.png';
-
+import React, { Component } from 'react'
+import AboutPopout from '../components/AboutPopout'
+import axios from 'axios'
+import brand from '../icons/TWOODLE.png'
+import about from '../icons/Howitworks.png'
 
 export default class Homepage extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       error: '',
       poppedOpen: false
     }
-   
     this.closePopup  = this.closePopup.bind(this)
-    this.submitForm = this.submitForm.bind(this);
-
+    this.submitForm = this.submitForm.bind(this)
   }
 
   openPopup = () => {
@@ -24,55 +20,52 @@ export default class Homepage extends Component {
       poppedOpen: true
     })
   }
-  
+
   closePopup = () => {
     this.setState({
       poppedOpen: false
     })
   }
 
-  submitForm = (event) => {
-    event.preventDefault();
-    event.persist();
-    const regex = /^[a-z0-9]+$/;
+  submitForm = event => {
+    event.preventDefault()
+    event.persist()
+    const regex = /^[a-z0-9]+$/
     let boardName = event.target.boardName.value;
-    boardName = boardName.toLowerCase().replace(' ','-');
+    boardName = boardName.toLowerCase()
 
      if (regex.test(boardName)) {
-
       axios.get(`http://localhost:3001/?boardName=${boardName}`)
-        .then((response) => {
-          window.location = `/twoodles/${boardName}`;
+        .then(response => {
+          window.location = `/twoodles/${boardName}`
         })
-        .catch((error) => {
+        .catch(error => {
           this.setState({error: 'Twoodle name is already taken. Please try another one.'});
-          this.refs.boardName.value="";
-        });
-
-     }
-     else {
+          this.refs.boardName.value=""
+        })
+     } else {
        this.setState({error: 'Please use alphanumerical characters'});
      }
   }
 
-  // NOTE: there should be a limited acceptable characters for the bordname
   render() {
-     return (
-     <div className='main-container'> 
-     {this.state.error ? 
-      <div className='error-message' >
-        <p>{this.state.error}</p>
-      </div> : ''}
-      <div className='jumbotron'>
-      <img className='home-logo' src={brand} />
-     
+    return (
+      <div className='main-container'>
+       {this.state.error ?
+        <div className='error-message' >
+          <p>{this.state.error}</p>
+        </div> : ''}
+        <div className='jumbotron'>
+        <img className='home-logo' src={brand} alt='brand' />
+
         <form className='new-twoodle'
         onSubmit={this.submitForm}>
           <input className='twoodle-name' name="boardName" ref='boardName'/>
           <button className='twoodle-submit'>submit</button>
-          <img className='about' onClick={this.openPopup} src={about} />
+          <img className='about' onClick={this.openPopup} src={about} alt='about' />
            <AboutPopout onClose={this.closePopup} isOpen={this.state.poppedOpen} />
         </form>
+
         <h2 className='slogan'>Welcome to your whiteboard <span className='purple'>on the web</span></h2>
         <footer className='footer'>
           <p className='copy'> ©2017 Twoodle Inc.</p>
@@ -85,6 +78,6 @@ export default class Homepage extends Component {
         </footer>
       </div>
     </div>
-    );
+    )
   }
 }
