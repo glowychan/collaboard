424232438135e4447926c48a30ebb13dd07e3c29
glowychan <glochan.uog@gmail.com>
2017-09-18 14:03:48 -0400

    clean eraser

--- src/components/tools/Eraser.js
+++ src/components/tools/Eraser.js
@@ -1,10 +1,10 @@
-import { v4 } from 'uuid';
+import { v4 } from 'uuid'
 
-export const TOOL_ERASER = 'eraser';
+export const TOOL_ERASER = 'eraser'
 
 export default (context) => {
-  let stroke = null;
-  let points = [];
+  let stroke = null
+  let points = []
 
 
   const onMouseDown = (x, y, color, size) => {
@@ -14,65 +14,64 @@ export default (context) => {
       color,
       size,
       points: [{ x, y}]
-    };
-    return [stroke];
-  };
+    }
+    return [stroke]
+  }
 
   const drawLine = (item, start, { x, y }) => {
-    context.save();
-    context.lineJoin = 'round';
-    context.lineCap = 'round';
-    context.beginPath();
-    context.lineWidth = 40;
-    context.strokeStyle = 'rgba(0,0,0,1)';
-    context.globalCompositeOperation = 'destination-out';
-    context.moveTo(start.x, start.y);
-    context.lineTo(x, y);
-    context.closePath();
-    context.stroke();
-    context.restore();
-  };
+    context.save()
+    context.lineJoin = 'round'
+    context.lineCap = 'round'
+    context.beginPath()
+    context.lineWidth = 40
+    context.strokeStyle = 'rgba(0,0,0,1)'
+    context.globalCompositeOperation = 'destination-out'
+    context.moveTo(start.x, start.y)
+    context.lineTo(x, y)
+    context.closePath()
+    context.stroke()
+    context.restore()
+  }
 
   const onMouseMove = (x, y) => {
-    if (!stroke) return [];
-    const newPoint = { x, y};
-    const start = stroke.points.slice(-1)[0];
-    drawLine(stroke, start, newPoint);
-    stroke.points.push(newPoint);
-    points.push(newPoint);
-
-    return [stroke];
-  };
+    if (!stroke) return []
+    const newPoint = { x, y}
+    const start = stroke.points.slice(-1)[0]
+    drawLine(stroke, start, newPoint)
+    stroke.points.push(newPoint)
+    points.push(newPoint)
+    return [stroke]
+  }
 
   const onDebouncedMouseMove = () => {
-    const debouncedPoints = points;
-    points = [];
-    return [stroke, debouncedPoints];
-  };
+    const debouncedPoints = points
+    points = []
+    return [stroke, debouncedPoints]
+  }
 
   const onMouseUp = (x, y) => {
-    if (!stroke) return;
-    onMouseMove(x, y);
-    points = [];
-    const item = stroke;
-    stroke = null;
-    return [item];
-  };
+    if (!stroke) return
+    onMouseMove(x, y)
+    points = []
+    const item = stroke
+    stroke = null
+    return [item]
+  }
 
   const draw = (item, animate) => {
-    let time = 0;
-    let i = 0;
-    const j = item.points.length;
+    let time = 0
+    let i = 0
+    const j = item.points.length
     for (i, j; i < j; i++) {
-      if (!item.points[i - 1]) continue;
+      if (!item.points[i - 1]) continue
       if (animate) {
-        setTimeout(drawLine.bind(null, item, item.points[i - 1], item.points[i]), time);
-        time += 10;
+        setTimeout(drawLine.bind(null, item, item.points[i - 1], item.points[i]), time)
+        time += 10
       } else {
-        drawLine(item, item.points[i - 1], item.points[i]);
+        drawLine(item, item.points[i - 1], item.points[i])
       }
     }
-  };
+  }
 
   return {
     onMouseDown,
@@ -80,5 +79,5 @@ export default (context) => {
     onDebouncedMouseMove,
     onMouseUp,
     draw,
-  };
-};
+  }
+}
