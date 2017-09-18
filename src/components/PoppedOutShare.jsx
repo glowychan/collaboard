a93fbdba356a08a55dc79279ba3acc35e256e752
glowychan <glochan.uog@gmail.com>
2017-09-18 14:36:16 -0400

    clean pop out share

--- src/components/PoppedOutShare.jsx
+++ src/components/PoppedOutShare.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { ShareButtons, generateShareIcon } from 'react-share';
 
 const FacebookIcon = generateShareIcon('facebook');
@@ -7,38 +7,29 @@ const EmailIcon = generateShareIcon('email');
 const LinkedinIcon = generateShareIcon('linkedin');
 
 const {
-            FacebookShareButton,
-            GooglePlusShareButton,
-            LinkedinShareButton,
-            TwitterShareButton,
-            TelegramShareButton,
-            WhatsappShareButton,
-            PinterestShareButton,
-            VKShareButton,
-            OKShareButton,
-            RedditShareButton,
-            EmailShareButton,
-          } = ShareButtons;
+        FacebookShareButton,
+        LinkedinShareButton,
+        TwitterShareButton,
+        EmailShareButton,
+      } = ShareButtons;
 
-
-class PoppedOutShare extends React.Component {
-  constructor(props) { 
-    super(props);
-
-    this.setWrapperRef = this.setWrapperRef.bind(this);           
-    this.handleClickOutside = this.handleClickOutside.bind(this);
+class PoppedOutShare extends Component {
+  constructor(props) {
+    super(props)
+    this.setWrapperRef = this.setWrapperRef.bind(this)
+    this.handleClickOutside = this.handleClickOutside.bind(this)
   }
-  
+
   componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    }
+    document.addEventListener('mousedown', this.handleClickOutside)
+  }
 
   componentWillUnmount() {
-      document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
   setWrapperRef(node) {
-      this.wrapperRef = node;
+      this.wrapperRef = node
     }
 
   handleClickOutside(event) {
@@ -46,42 +37,36 @@ class PoppedOutShare extends React.Component {
           this.props.onClose()
       }
   }
-   
+
   render() {
-    const URL = "https://twoodle-board.com/" + this.props.url
-    
-    if(this.props.isOpen === false)
-    return null 
+    const URL = `https://twoodle-board.com/${this.props.url}`
+
+    if(!this.props.isOpen)
+    return null
 
     return (
       <div ref={this.setWrapperRef} className='share-pop'>
         <div className='url-share'>
           <h2>Share your Twoodle URL and draw with friends</h2>
-          <input className='share-url' id='share' type='text' defaultValue={'https://twoodle-board.com/' + this.props.url} readOnly /> 
+          <input className='share-url' id='share' type='text' defaultValue={URL} readOnly />
         </div>
-      
-      
-      <div className='social-share'>
-      
-        <FacebookShareButton title="Twoodle" quote="Check out my latest Twoodle" picture='https://static.tumblr.com/a084c14f08f0fa56a12f1c79fc90aa16/sl6w7fs/qQHockr0d/tumblr_static_tumblr_static_axzrmvvucm0c80wwwwc8g8kko_640.jpg' url={URL}>
-          <FacebookIcon size={60} round />
-        </FacebookShareButton>
-
-        <TwitterShareButton title="Check out my latest Twoodle" url={'https://twoodle-board.com/' + this.props.url} hashtags={['Twoodle', 'CollaborationTools']}>
-          <TwitterIcon size={60} round/>
-        </TwitterShareButton>
-
-        <LinkedinShareButton title="Check out my latest Twoodle" url={'https://twoodle-board.com/' + this.props.url}> 
-          <LinkedinIcon size={60} round/>
-        </LinkedinShareButton>
-
-        <EmailShareButton subject="Let's collaborate! Join my Twoodle Board" body={"Lets visualize our ideas. Check out my latest Twoodle Board here: https://twoodle-board.com/" + this.props.url}>
-          <EmailIcon size={60} round/>
-        </EmailShareButton>
+        <div className='social-share'>
+          <FacebookShareButton title="Twoodle" quote="Check out my latest Twoodle" picture='https://static.tumblr.com/a084c14f08f0fa56a12f1c79fc90aa16/sl6w7fs/qQHockr0d/tumblr_static_tumblr_static_axzrmvvucm0c80wwwwc8g8kko_640.jpg' url={URL}>
+            <FacebookIcon size={60} round />
+          </FacebookShareButton>
+          <TwitterShareButton title="Check out my latest Twoodle" url={URL} hashtags={['Twoodle', 'CollaborationTools']}>
+            <TwitterIcon size={60} round/>
+          </TwitterShareButton>
+          <LinkedinShareButton title="Check out my latest Twoodle" url={URL}>
+            <LinkedinIcon size={60} round/>
+          </LinkedinShareButton>
+          <EmailShareButton subject="Let's collaborate! Join my Twoodle Board" body={"Lets visualize our ideas. Check out my latest Twoodle Board here: " + URL}>
+            <EmailIcon size={60} round/>
+          </EmailShareButton>
+        </div>
       </div>
-    </div>
     )
   }
 }
 
-export default PoppedOutShare 
+export default PoppedOutShare
