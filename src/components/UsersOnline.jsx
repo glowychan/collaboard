bdf40cef1b97eddf0456b34bbaba3eea2830e2c2
glowychan <glochan.uog@gmail.com>
2017-09-18 15:37:11 -0400

    clean users online

--- src/components/UsersOnline.jsx
+++ src/components/UsersOnline.jsx
@@ -1,30 +1,13 @@
 import React from 'react'
 
-
-
-class UsersOnline extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  // randomColor = () => {
-  //  return '#' + (0x1000000 + Math.random() * 0xFFFFFF).toString(16).substr(1,6);
-  // }
-
-
-
-  render() {
-    const users = this.props.users
-    return (
-    <div className='user-container'>
-      {users.map((user) => {
-        return <div className='online-user' style={{background: user.color}} key={user.id}>
-          <p> {user.name} </p>
-        </div>
-      })}
-    </div>
-    );
-  }
-}
+const UsersOnline = ({ users }) => (
+  <div className='user-container'>
+    {users.map((user) => {
+      return <div className='online-user' style={{background: '#6ED3CF'}} key={user.id}>
+        <p> {user.name} </p>
+      </div>
+    })}
+  </div>
+)
 
 export default UsersOnline
