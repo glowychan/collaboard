33b1d13c4ba67d99d8cb6053602b1783d6c4aa5c
glowychan <glochan.uog@gmail.com>
2017-09-18 14:22:16 -0400

    clean about popup

--- src/components/AboutPopout.jsx
+++ src/components/AboutPopout.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import about from '../icons/About.png'
 
-class AboutPopout extends React.Component {
-  constructor(props) { 
-    super(props);
-
-    this.setWrapperRef = this.setWrapperRef.bind(this);           
+class AboutPopout extends Component {
+  constructor(props) {
+    super(props)
+    this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
   }
-  
+
   componentDidMount() {
-      document.addEventListener('mousedown', this.handleClickOutside);
+      document.addEventListener('mousedown', this.handleClickOutside)
     }
 
   componentWillUnmount() {
-      document.removeEventListener('mousedown', this.handleClickOutside);
+      document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
   setWrapperRef(node) {
-      this.wrapperRef = node;
+      this.wrapperRef = node
     }
 
   handleClickOutside(event) {
@@ -26,15 +25,13 @@ class AboutPopout extends React.Component {
           this.props.onClose()
       }
   }
-   
-  render() {
-    
-    if(this.props.isOpen === false)
-    return null 
 
+  render() {
+    if(!this.props.isOpen)
+    return null
     return (
       <div ref={this.setWrapperRef} className='about-pop'>
-         <img src={about} />
+         <img src={about} alt='about' />
     </div>
     )
   }
