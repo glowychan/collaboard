25561487dd78d6ba466ddc6a691d1c2b22b107a2
glowychan <glochan.uog@gmail.com>
2017-09-18 15:41:04 -0400

    clean webrtc

--- src/components/WebRTC.jsx
+++ src/components/WebRTC.jsx
@@ -1,20 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SimpleWebRTC from 'simplewebrtc'
 import Draggable from 'react-draggable'
 
-
-class WebRTC extends React.Component {
-  constructor(props) { 
+class WebRTC extends Component {
+  constructor(props) {
     super(props);
     this.state = ({
       videoOn: true
     })
-
    this.toggleVideo = this.toggleVideo.bind(this)
   }
-
-
-
 
   componentDidMount() {
     this.webrtc = new SimpleWebRTC({
@@ -24,24 +19,20 @@ class WebRTC extends React.Component {
       remoteVideosEl: 'remoteVideos',
       // immediately ask for camera access
       autoRequestMedia: true,
-
       localVideo: {
         autoplay: true, // automatically play the video stream on the page
         muted: true // mute local video stream to prevent echo
       },
-
       media: {audio: true, video: true}
-     
-    });
+    })
 
-        this.webrtc.on('readyToCall', () => {
-            
-           this.webrtc.joinRoom(this.props.roomName + 'gra091873');
-          });
-    }
-    
-    toggleVideo = () => {
-    if (this.state.videoOn === true) {
+    this.webrtc.on('readyToCall', () => {
+      this.webrtc.joinRoom(this.props.roomName + 'gra091873')
+    })
+  }
+
+  toggleVideo = () => {
+    if (this.state.videoOn) {
       this.webrtc.pauseVideo()
       this.setState({
         videoOn: false
@@ -53,20 +44,17 @@ class WebRTC extends React.Component {
       })
     }
   }
-  
-  
 
   render () {
     return (
-    <div className='chatbar'>
-     <video id="localVideo" onClick={this.toggleVideo}></video>
-      <Draggable>
-      <div id="remoteVideos"></div>
-      </Draggable>
-    </div>
+      <div className='chatbar'>
+       <video id="localVideo" onClick={this.toggleVideo}></video>
+        <Draggable>
+        <div id="remoteVideos"></div>
+        </Draggable>
+      </div>
     )
   }
-
 }
 
-export default WebRTC;
+export default WebRTC
