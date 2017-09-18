0713ea18d43f7037e7ab7dd29ccd0fec0eb0c553
glowychan <glochan.uog@gmail.com>
2017-09-18 14:05:17 -0400

    clean line

--- src/components/tools/Line.js
+++ src/components/tools/Line.js
@@ -1,10 +1,10 @@
-import { v4 } from 'uuid';
+import { v4 } from 'uuid'
 
-export const TOOL_LINE = 'line';
+export const TOOL_LINE = 'line'
 
 export default (context) => {
-  let line = null;
-  let imageData = null;
+  let line = null
+  let imageData = null
 
   const onMouseDown = (x, y, color, size) => {
     line = {
@@ -14,48 +14,48 @@ export default (context) => {
       size,
       start: { x, y },
       end: null
-    };
-    imageData = context.getImageData(0, 0, context.canvas.clientWidth, context.canvas.clientHeight);
-    return [line];
-  };
+    }
+    imageData = context.getImageData(0, 0, context.canvas.clientWidth, context.canvas.clientHeight)
+    return [line]
+  }
 
   const drawLine = (item, x, y) => {
-    context.save();
-    context.lineJoin = 'round';
-    context.lineCap = 'round';
-    context.beginPath();
-    context.lineWidth = item.size;
-    context.strokeStyle = item.color;
-    context.globalCompositeOperation = 'source-over';
-    context.moveTo(item.start.x, item.start.y);
-    context.lineTo(x, y);
-    context.closePath();
-    context.stroke();
-    context.restore();
-  };
+    context.save()
+    context.lineJoin = 'round'
+    context.lineCap = 'round'
+    context.beginPath()
+    context.lineWidth = item.size
+    context.strokeStyle = item.color
+    context.globalCompositeOperation = 'source-over'
+    context.moveTo(item.start.x, item.start.y)
+    context.lineTo(x, y)
+    context.closePath()
+    context.stroke()
+    context.restore()
+  }
 
   const onMouseMove = (x, y) => {
-    if (!line) return;
-    context.putImageData(imageData, 0, 0);
-    drawLine(line, x, y);
-  };
+    if (!line) return
+    context.putImageData(imageData, 0, 0)
+    drawLine(line, x, y)
+  }
 
   const onMouseUp = (x, y) => {
-    if (!line) return;
-    onMouseMove(x, y);
-    const item = line;
-    imageData = null;
-    line = null;
-    item.end = { x, y };
-    return [item];
-  };
+    if (!line) return
+    onMouseMove(x, y)
+    const item = line
+    imageData = null
+    line = null
+    item.end = { x, y }
+    return [item]
+  }
 
-  const draw = item => drawLine(item, item.end.x, item.end.y);
+  const draw = item => drawLine(item, item.end.x, item.end.y)
 
   return {
     onMouseDown,
     onMouseMove,
     onMouseUp,
     draw,
-  };
-};
+  }
+}
