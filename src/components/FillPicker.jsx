bff95b20a310fb18fca261819166a2fa03c42372
glowychan <glochan.uog@gmail.com>
2017-09-18 14:27:33 -0400

    clean fill picker

--- src/components/FillPicker.jsx
+++ src/components/FillPicker.jsx
@@ -1,29 +1,28 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import { CirclePicker } from 'react-color'
 
-class FillPicker extends React.Component {
+class FillPicker extends Component {
   state = {
     background: '#fff',
-  };
+  }
 
-  handleChangeComplete = (color) => {
-    this.setState({ background: color.hex });
+  handleChangeComplete = color => {
+    this.setState({ background: color.hex })
     this.props.newFill(this.state.background)
-  };
+  }
 
-    render() {
+  render() {
     return (
       <CirclePicker
-        color={ this.state.background } 
-        width={145} 
+        color={this.state.background}
+        width={145}
         circleSize={13}
-        colors={["#FF0000", "#FFC300", "#529749", "#2196f3",  "#000000"]}
-        onChangeComplete={ this.handleChangeComplete }
+        colors={["#FF0000", "#FFC300", "#529749", "#2196f3", "#000000"]}
+        onChangeComplete={this.handleChangeComplete}
       />
-    );
+    )
   }
 }
-
 
 export default FillPicker
