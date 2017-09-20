b6e848266992c9d0c682e8c51745ed2e2969226d
notadeejay <amy@lighthouselabs.ca>
2017-09-20 11:38:47 -0400

    Ran npm build

--- src/components/UsersOnline.jsx
+++ src/components/UsersOnline.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UsersOnline = ({ users }) => (
   <div className='user-container'>
     {users.map((user) => {
-      return <div className='online-user' style={{background: '#6ED3CF'}} key={user.id}>
+      return <div className='online-user' style={{background: user.color}} key={user.id}>
         <p> {user.name} </p>
       </div>
     })}
